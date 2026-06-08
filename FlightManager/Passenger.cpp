86101b34a4b6ddf9aceefda4bee4624c2c22e6d1
juingyeong <jg10161610@gmail.com>
2026-06-08 13:06:24 +0900

    getter은 inline으로

--- FlightManager/Passenger.cpp
+++ FlightManager/Passenger.cpp
@@ -9,7 +9,7 @@ Passenger::Passenger(string name, int age, string passportNo)
 {
 	this->name = name;
 	this->age = age;
-	this->passPortNo = passportNo;
+	this->passportNo = passportNo;
 }
 
 void Passenger::printInfo() {			//  승객 정보 등록 확인용
@@ -17,17 +17,13 @@ void Passenger::printInfo() {			//  승객 정보 등록 확인용
 	cout << " " << name << "님의 정보가 정상적으로 등록되었습니다." << endl;
 	cout << " 이름: " << name << endl;
 	cout << " 나이: " << age << endl;
-	cout << " 여권번호: " << passPortNo << endl;
+	cout << " 여권번호: " << passportNo << endl;
 	cout << "-------------------------------------------" << endl;
 }
 
 void Passenger::printTicket() {			// E-Ticket에서 승객 정보 출력
 	cout << " 승객명: " << name << endl;
-	cout << " 여권번호: " << passPortNo << endl;
-}
-
-string Passenger::getPassportNo() {		// 여권 번호 반환
-	return passPortNo;
+	cout << " 여권번호: " << passportNo << endl;
 }
 
 void Passenger::setSelectedFlight(int selectedFlight) {	// 항공편 선택
--- FlightManager/Passenger.h
+++ FlightManager/Passenger.h
@@ -7,7 +7,7 @@ using namespace std;
 class Passenger {
 private:
 	int age;			// 승객 나이
-	string passPortNo;	// 여권번호
+	string passportNo;	// 여권번호
 
 	int selectedFlight;		// 선택한 항공편
 	int selectedSeatClass;		// 선택한 좌석 등급
@@ -19,14 +19,14 @@ public:
 
 	void printInfo();		//  승객 정보 등록 확인용
 	void printTicket();		// E-Ticket에서 승객 정보 출력
-	string getPassportNo();	// 여권번호 반환
 
 	void setSelectedFlight(int selectedFlight);		// 항공편 선택
 	void setSelectedSeatClass(int selectedSeatClass);	// 좌석 등급 선택
 	void setSelectedSeat(string selectedSeat);	// 좌석 선택
 
+	inline string getPassportNo() { return passportNo; }			// 여권번호 반환
 	inline int getSelectedFlight() { return selectedFlight; }		// 선택한 항공편 반환
-	inline int getSelectedSeatClass() { return selectedSeatClass; }		// 선택한 좌석 등급 반환
-	string getSelectedSeat() { return selectedSeat; }		// 선택한 좌석 반환
+	inline int getSelectedSeatClass() { return selectedSeatClass; }	// 선택한 좌석 등급 반환
+	inline string getSelectedSeat() { return selectedSeat; }		// 선택한 좌석 반환
 };
 #endif // !PASSENGER_H
